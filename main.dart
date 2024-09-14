9d023bb972a0995b9616fbd84d6135b1335e2853
Pedro-Kukul <pkukul411@gmail.com>
2024-09-13 20:41:41 -0600

    Added some example for user input

--- main.dart
+++ main.dart
@@ -1,7 +1,19 @@
-void main() { 
-  print('baslangic');
-  for (int i = 0; i<5; i++){
-    print('hello ${i+1}');
+import 'dart:io';
+
+void main() {
+  while (true) {
+    stdout.write('Enter your name? ');
+    late String? input = stdin.readLineSync();
+
+    /// Read user input
+    if (input == "HALT" || input == "halt") {
+      print("Program EXITED");
+      break;
+    }
+
+    /// Process the valid input
+    print('Hello, $input! \n\n');
+
+    /// Example of handling valid input
   }
-  print('bitis');
 }
